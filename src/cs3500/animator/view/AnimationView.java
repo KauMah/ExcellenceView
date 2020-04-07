9a758d15c30286c6dd7fe3e7241f7f7b5e59e5ea
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2020-04-07 15:13:59 -0400

    added buttons to new panel

--- src/cs3500/animator/view/AnimationView.java
+++ src/cs3500/animator/view/AnimationView.java
@@ -1,5 +1,16 @@
 package cs3500.animator.view;
 
-public interface AnimationView {
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
+public interface AnimationView {
+  /**
+   * Set the listener for any actions.
+   */
+  void setListener(ActionListener listener);
+
+  /**
+   * Display this view.
+   */
+  void display();
 }
